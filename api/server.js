171e12115cb84c13dfb14f0086d4d175e0e41705
Larkin Campbell <Larkincampbell24@gmail.com>
2024-03-24 16:08:11 -0700

    wip

--- api/server.js
+++ api/server.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const server = express()
-
+const jokesRouter = require('./jokesRouter')
 
 server.use(express.json())
+server.use('/jokes', jokesRouter)
 
 module.exports = server
